48e94d3e04e7515badeac8decc76bd98d3a96476
建军 <lijianjun@yy.com>
2018-06-24 23:12:05 +0800

    增加创建Component或者Page以及目录文件

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 * 安装node环境
 * clone 本目录之后在根目录执行`npm link`安装到全局
-* 执行`wxapp [,filename] [-s]`
+* 执行`wxapp [,filename] [-s]|[-c]`
 
 ## 创建文件
 如果第2个参数没有的话，默认使用目录名作为文件名新增4个文件，例如：
@@ -14,6 +14,17 @@
 如果第2个参数有的话，使用参数作为文件名新增4个文件，例如：
 `a/b/c`在`c`目录下执行`wxapp d`，那么就会创建`d.wxml` `d.json` `d.wxss` `d.json`
 
+## 创建目录并创建同名文件
+
+`wxapp /[filename]`例如：
+`wxapp /c -s` 则会在当前目录下面创建c文件目录以及c文件
+`wxapp /c/d/e -s` 则会在`/c/d/e`下面创建对应的文件
+
 ## 创建scss文件
 
 `wxapp -s` OR `wxapp [,fileName] -s`
+
+
+## 创建Component文件
+
+`wxapp -c` 创建Component，文件内容为`src/component.js`
--- bin/index.js
+++ bin/index.js
@@ -7,12 +7,31 @@ let file_path = '';
 let suffix = '';
 let cmd2 = process.argv[2];
 let cmd3 = process.argv[3];
+let cmd4 = process.argv[4];
+let pageFile = path.join(__dirname, '../src/page.js');
+let componentFile = path.join(__dirname, '../src/component.js');
+let rs = '';
+
 let createFile = (fileName, suffix, path) => {
   if(!fileName) return;
   fs.writeFileSync(`${ path + fileName }.${ suffix || 'wxss' }`, '',)
   fs.writeFileSync(`${ path + fileName }.wxml`, '', )
   fs.writeFileSync(`${ path +fileName }.json`, '{}', )
-  fs.writeFileSync(`${ path + fileName }.js`, '', )
+  
+  
+  // fs.readFile(pageFile, 'utf8', (error, data) => {
+  //   if(error) throw err;
+  //   fs.writeFileSync(`${ path + fileName }.js`, 'Page({\nonLoad(){\n\n}\n})', )
+  // })
+  if(cmd3 == '-c' || cmd4 == '-c') {
+    rs = fs.createReadStream(componentFile)
+  } else {
+    rs = fs.createReadStream(pageFile);
+  }
+  let ws = fs.createWriteStream(`${ path + fileName }.js`);
+  rs.pipe(ws, { end: false });
+
+  console.log(`${ fileName }文件创建成功`)
 }
 
 let syncCreateFile = (dirname) => {
